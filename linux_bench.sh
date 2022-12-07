59536bfa531acff86904988006a70615e7f9936b
Tom Madureira <madureiratom@gmail.com>
2022-12-07 09:59:26 -0700

    Create linux_bench.sh

---
+++ linux_bench.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# make pipelines' return status equal the last command to exit with a non-zero status, or zero if all commands exit successfully
+set -o pipefail
+# debug log
+# set -x
+brew install sysbench >/dev/null
+MAX_PRIME=20000
+echo "==CPUBenchmark_SingleCore"
+CPUS=$(sysctl -n hw.ncpu)
+sysbench cpu run --cpu-max-prime=20000 --time=20 |grep "events per second:"
+echo "==CPUBenchmark_MultiCore($CPUS)"
+sysbench cpu run --threads=$CPUS --cpu-max-prime=20000 --time=20 |grep "events per second:"
+if [[ "$(sysctl -n machdep.cpu.brand_string)" =~ "Apple M1 Max" ]]; then
+  echo "==CPUBenchmark_MultiCore(8)"
+  [ $CPUS -eq 10 ] &&  sysbench cpu run --threads=8 --cpu-max-prime=20000 --time=20 |grep "events per second:" || echo " events per second: 0"
+  echo "==CPUBenchmark_MultiCore(4)"
+  sysbench cpu run --threads=4 --cpu-max-prime=20000 --time=20 |grep "events per second:"
+fi
+
+echo "==MemBenchmark_SingleCore"
+sysbench memory run --threads=1 | grep "MiB transferred"
+echo "==MemBenchmark_MultiCore($CPUS)"
+sysbench memory run --threads=$CPUS | grep "MiB transferred"
+if [[ "$(sysctl -n machdep.cpu.brand_string )" =~ "Apple M1 Max" ]]; then
+  echo "==MemBenchmark_MultiCore(8)"
+  [ $CPUS -eq 10 ] && sysbench memory run --threads=8 | grep "MiB transferred" || echo "0.0 MiB transferred (0.0 MiB/sec)"
+  echo "==MemBenchmark_MultiCore(4)"
+  sysbench memory run --threads=4 | grep "MiB transferred"
+fi
+
+echo "==FileIOBenchmark_MultiCoreCacheDriven($CPUS)"
+#sysbench fileio prepare --file-total-size=128G --file-num=2 >/dev/null
+sysbench fileio prepare >/dev/null
+sysbench fileio run --threads=$CPUS --file-test-mode=seqrewr | grep "written, MiB/s:"  # --max-time=300 --file-num=2 --file-total-size=128G
+sysbench fileio cleanup >/dev/null
+sysbench fileio prepare >/dev/null
+if [[ "$(sysctl -n machdep.cpu.brand_string)" =~ "Apple M1 Max" ]]; then
+  echo "==FileIOBenchmark_MultiCoreCacheDriven(8)"
+  [ $CPUS -eq 10 ] && sysbench fileio run --threads=8 --file-test-mode=seqrewr | grep "written, MiB/s:" || echo "written, MiB/s:               0.0"
+  sysbench fileio cleanup >/dev/null
+  sysbench fileio prepare >/dev/null
+  echo "==FileIOBenchmark_MultiCoreCacheDriven(4)"
+  sysbench fileio run --threads=4 --file-test-mode=seqrewr | grep "written, MiB/s:"
+fi
